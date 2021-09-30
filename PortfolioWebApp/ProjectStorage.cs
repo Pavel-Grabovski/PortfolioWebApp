64c8c50076a024fea5536f3f9c67875bd7df5674
Pavel <araa9977@mail.ru>
2021-09-30 22:36:56 +0700

    two commit

---
+++ PortfolioWebApp/ProjectStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioWebApp
+{
+
+    public static class ProjectStorage
+    {
+        public static List<Project> Projects { get; private set; } = new List<Project>()
+        {
+            new Project
+            {
+                Name = "ASP.NET_Core_1.0",
+                Link = "https://github.com/Pavel-Grabovski/PortfolioWebApp",
+                Description = "Написание WEB приложений с помощью ASP.NET."
+            }
+        };
+    }
+}
